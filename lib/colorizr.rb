60bc26f85ec38a9a9e6ff644ab31bb05ff72b1eb
Femi Ajayi <femiajayi705@gmail.com>
2016-08-02 03:33:13 +0800

    Refactored the gem to reduce repetition with the colorcode hash

--- lib/colorizr.rb
+++ lib/colorizr.rb
@@ -1,26 +1,24 @@
 class String
-	colors_hash = {black: 30, red: 31, green: 32, yellow:33, blue: 34, pink: 35, light_gray: 37, light_blue: 94, white: 97}
-
+	#Class variable containing the colors and codes
+	@@colors_hash = {black: 30, red: 31, green: 32, yellow:33, blue: 34, pink: 35, light_gray: 37, light_blue: 94, white: 97}
+	# Define the class method to display the list of colors
 	def self.colors
-		colors_hash = {black: 30, red: 31, green: 32, yellow:33, blue: 34, pink: 35, light_gray: 37, light_blue: 94, white: 97}
-		colors_hash.keys
+		@@colors_hash.keys
 	end
-
+	# Define the class method for the sample colors
 	self.instance_eval do
 		def sample_colors
-			colors_hash = {black: 30, red: 31, green: 32, yellow:33, blue: 34, pink: 35, light_gray: 37, light_blue: 94, white: 97}
-			colors_hash.each {|key,value| puts("This is \e[#{value}m#{key}\e[0m")}
+			@@colors_hash.each {|key,value| puts("This is \e[#{value}m#{key}\e[0m")}
 		end
 	end
-
+	# Define the instace method for the colors
 	def self.create_colors
-		colors_hash = {black: 30, red: 31, green: 32, yellow:33, blue: 34, pink: 35, light_gray: 37, light_blue: 94, white: 97}
-		colors_hash.each { |key,value|
+		@@colors_hash.each { |key,value|
 			define_method("#{key}") do
-				puts "\e[#{value}m#{self}\e[0m"
+				"\e[#{value}m#{self}\e[0m"
 			end
 		}
 	end
-
+	# Create the instance methods for the colors
 	create_colors
 end
